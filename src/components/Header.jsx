bf8d38107661bfed16b85c35a6cf21fd6c79e240
Parshuram Nikam <parshunikam0612@gmail.com>
2022-02-25 11:35:23 +0530

    initial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Header = () => {
-  return (
-    <header className='h-screen bg-emerald-400 relative overflow-hidden '>
-    <img src="images/header/wave-1.svg" alt="" />
-    <img src="images/header/wave-2.svg" alt="" />
-    <img src="images/header/wave-3.svg" alt="" />
-    <img src="images/header/wave-4.svg" alt="" />
-    <img className='absolute left-0 bottom-0 ' src="wave-5.svg" alt="" />
-  </header>
-  )
+    return (
+        <header className='h-screen bg-emerald-400 relative overflow-hidden '>
+
+            <Navbar />
+
+            <img className='header_wave' src="images/header/wave-1.svg" alt="" />
+            <img className='header_wave' src="images/header/wave-2.svg" alt="" />
+            <img className='header_wave' src="images/header/wave-3.svg" alt="" />
+            {/* <img className='header_wave' src="images/header/wave-3.svg" alt="" /> */}
+            <img className='header_wave absolute left-0 bottom-0 ' src="wave-5.svg" alt="" />
+        </header>
+    )
 }
 
 export default Header
---
+++ src/constants/navItems.js
@@ -0,0 +1,7 @@
+
+export const navigation = [
+    { name: 'Home', href: '/', current: true },
+    { name: 'Team', href: '/#', current: false },
+    { name: 'Projects', href: '/#', current: false },
+    { name: 'Calendar', href: '/#', current: false },
+]
